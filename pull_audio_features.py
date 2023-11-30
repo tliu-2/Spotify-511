eb1807ea059870ecca0eef3f587fd3ed40b4cd50
tliu-2 <tliu2@uw.edu>
2023-11-30 15:47:38 -0800

    Add fonts and throttle to pull audio wip

--- pull_audio_features.py
+++ pull_audio_features.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.exceptions import SpotifyException
 import glob
 import time
+import requests
 import ast
 
 
 # Insert your client id and client secret from the Spotify Dashboard View.
-Client_ID = 'None'
-Client_Secret = 'None'
+Client_ID = ''
+Client_Secret = ''
 
 
 def expand_artist_info(row, max_artists=3):
@@ -36,84 +38,186 @@ def extract_album_details(row):
                      index=['album_name', 'album_id', 'album_uri', 'album_type'])
 
 
-
-def pull_song_features(splice_list):
+def pull_song_features(splice_list, market='US'):
     df_list = []
-    i = 0
+    request_count = 0
+    start_time = time.time()
+    length_splice = len(splice_list)
+    pos = 0
     for x in splice_list:
-        # Sleep to prevent going over the API rate-limit.
-        # time.sleep(35)
-        # Pull audio features from Spotify API.
-        track_features = sp.tracks(tracks=x)
+        print(f'Song Feature: @ {pos} out of {length_splice}')
+        if len(x) == 0:
+            continue
 
-        # Convert the features to a Dataframe.
-        results_df = pd.DataFrame(track_features['tracks'])
-        results_df = results_df.rename(columns={'id': 'raw_track_uri'})
+        success = False
+        while not success:
+            try:
+                current_time = time.time()
+                elapsed_time = current_time - start_time
 
-        # Output the audio features for the current splice to a csv.
-        # Append the combined splice to a list.
-        df_list.append(results_df)
-        i += 1
+                # Reset every 30 seconds
+                if elapsed_time >= 30:
+                    start_time = current_time
+                    request_count = 0
 
-    # Concat the list of dataframes to one big dataframe.
-    final_df = pd.concat(df_list)
-    return final_df
+                # Throttle requests after 180 in a 30-second window
+                if request_count >= 180:
+                    time.sleep(30)  # Sleep for 30 seconds to throttle
+                    start_time = time.time()
+                    request_count = 0
+
+                track_features = sp.tracks(tracks=x, market=market)
+                request_count += 1
+
+                if track_features and 'tracks' in track_features:
+                    results_df = pd.DataFrame(track_features['tracks'])
+                    results_df = results_df.rename(columns={'id': 'raw_track_uri'})
+                    df_list.append(results_df)
+                    success = True
+            except SpotifyException as e:
+                if e.http_status == 429:
+                    retry_after = e.headers.get('Retry-After', 1)  # Use the Retry-After header if available
+                    print(f"Rate limit exceeded. Retrying after {retry_after} seconds.")
+                    time.sleep(int(retry_after))
+                else:
+                    print(f"Spotify API error: {e}")
+                    break  # or handle the error as needed
+            except Exception as general_exception:
+                print(f"An error occurred: {general_exception}")
+                break  # or handle the error as needed
+        pos += 1
+    if df_list:
+        final_df = pd.concat(df_list)
+        return final_df
+    else:
+        return pd.DataFrame()
 
 
 def pull_artists(splice_list):
     df_list = []
-    i = 0
+    request_count = 0
+    start_time = time.time()
+    length_splice = len(splice_list)
+    pos = 0
     for x in splice_list:
-        # Sleep to prevent going over the API rate-limit.
-        # time.sleep(35)
-        # Pull audio features from Spotify API.
-        artists = sp.artists(artists=x)
+        print(f'Artist Feature: @ {pos} out of {length_splice}')
+        if len(x) == 0:
+            continue
 
-        # Convert the features to a Dataframe.
-        results_df = pd.DataFrame(artists['artists'])
-        results_df = results_df.rename(columns={'id': 'raw_artist_uri'})
+        success = False
+        while not success:
+            try:
+                current_time = time.time()
+                elapsed_time = current_time - start_time
 
-        # Output the audio features for the current splice to a csv.
-        # results_df.to_csv(f'./data/spotify_api_pulls/artists_{new_fname}_{i}.csv')
+                # Reset every 30 seconds
+                if elapsed_time >= 30:
+                    start_time = current_time
+                    request_count = 0
 
-        # Merge together the playlist data and the audio features on the raw_track_uri.
-        combined_df = pd.merge(df, results_df, on=['raw_artist_uri'], how='inner')
-        combined_df = combined_df.sort_values(by=['pid', 'pos'])
-        # Append the combined splice to a list.
-        df_list.append(results_df)
-        i += 1
+                # Throttle requests after 180 in a 30-second window
+                if request_count >= 180:
+                    time.sleep(30)  # Sleep for 30 seconds to throttle
+                    start_time = time.time()
+                    request_count = 0
 
-    # Concat the list of dataframes to one big dataframe.
-    final_df = pd.concat(df_list)
-    return final_df
+                response = sp.artists(artists=x)
+                request_count += 1
+
+                if response:
+                    results_df = pd.DataFrame(response)
+                    results_df = results_df.rename(columns={'id': 'raw_artist_uri'})
+                    df_list.append(results_df)
+                    success = True
+            except SpotifyException as e:
+                if e.http_status == 429:
+                    retry_after = e.headers.get('Retry-After', 1)  # Use the Retry-After header if available
+                    print(f"Rate limit exceeded. Retrying after {retry_after} seconds.")
+                    time.sleep(int(retry_after))
+                else:
+                    print(f"Spotify API error: {e}")
+                    break  # or handle the error as needed
+            except Exception as general_exception:
+                print(f"An error occurred: {general_exception}")
+                break  # or handle the error as needed
+        pos += 1
+    if df_list:
+        final_df = pd.concat(df_list)
+        return final_df
+    else:
+        return pd.DataFrame()
 
 
 def pull_audio_features(splice_list):
     df_list = []
-    i = 0
+    request_count = 0
+    start_time = time.time()
+    length_splice = len(splice_list)
+    pos = 0
     for x in splice_list:
-        # Sleep to prevent going over the API rate-limit.
-        # time.sleep(35)
-        # Pull audio features from Spotify API.
-        song_audio_features = sp.audio_features(tracks=x)
+        print(f'Audio Feature: @ {pos} out of {length_splice}')
+        if len(x) == 0:
+            continue
 
-        # Convert the features to a Dataframe.
-        results_df = pd.DataFrame(song_audio_features)
-        results_df = results_df.rename(columns={'id': 'raw_track_uri'})
+        success = False
+        while not success:
+            try:
+                current_time = time.time()
+                elapsed_time = current_time - start_time
 
-        # Output the audio features for the current splice to a csv.
-        # results_df.to_csv(f'./data/spotify_api_pulls/song_features_{new_fname}_{i}.csv')
+                # Reset every 30 seconds
+                if elapsed_time >= 30:
+                    start_time = current_time
+                    request_count = 0
 
-        # Merge together the playlist data and the audio features on the raw_track_uri.
-        combined_df = pd.merge(df, results_df, on=['raw_track_uri'], how='inner')
-        combined_df = combined_df.sort_values(by=['pid', 'pos'])
-        # Append the combined splice to a list.
-        df_list.append(results_df)
-        i += 1
+                # Throttle requests after 180 in a 30-second window
+                if request_count >= 180:
+                    time.sleep(30)  # Sleep for 30 seconds to throttle
+                    start_time = time.time()
+                    request_count = 0
 
-    # Concat the list of dataframes to one big dataframe.
-    final_df = pd.concat(df_list)
-    return final_df
+                response = sp.audio_features(tracks=x)
+                request_count += 1
+
+                if response:
+                    results_df = pd.DataFrame(response)
+                    results_df = results_df.rename(columns={'id': 'raw_track_uri'})
+                    df_list.append(results_df)
+                    success = True
+            except SpotifyException as e:
+                if e.http_status == 429:
+                    retry_after = e.headers.get('Retry-After', 1)  # Use the Retry-After header if available
+                    print(f"Rate limit exceeded. Retrying after {retry_after} seconds.")
+                    time.sleep(int(retry_after))
+                else:
+                    print(f"Spotify API error: {e}")
+                    break  # or handle the error as needed
+            except Exception as general_exception:
+                print(f"An error occurred: {general_exception}")
+                break  # or handle the error as needed
+        pos += 1
+    if df_list:
+        final_df = pd.concat(df_list)
+        return final_df
+    else:
+        return pd.DataFrame()
+
+
+
+def generate_splices(data_list, batch_size):
+    for i in range(0, len(data_list), batch_size):
+        yield data_list[i:i + batch_size]
+
+
+def extract_spotify_id(uri):
+    parts = uri.split(':')
+    if len(parts) == 3 and parts[0] == 'spotify':
+        return parts[2]
+    else:
+        # Handle the case where the URI is not in the expected format
+        # This could be logging an error, returning None, or some other action
+        print(f"Unexpected URI format: {uri}")
+        return None
 
 
 if __name__ == '__main__':
@@ -122,82 +226,44 @@ if __name__ == '__main__':
     for fname in glob.glob(path):
         print(f'Processing {fname}\n')
         df = pd.read_parquet(fname)
-        df['raw_track_uri'] = df['track_uri'].str.split(':')
-        df['raw_track_uri'] = df['raw_track_uri'].str[2]
-        df['raw_artist_uri'] = df['artist_uri'].str.split(':')
-        df['raw_artist_uri'] = df['raw_artist_uri'].str[2]
+        df['raw_track_uri'] = df['track_uri'].apply(extract_spotify_id)
+        df['raw_artist_uri'] = df['artist_uri'].apply(extract_spotify_id)
 
-        unique_songs = df[['track_name', 'artist_name', 'artist_uri', 'track_uri', 'raw_track_uri', 'raw_artist_uri']]
-        unique_songs = unique_songs['raw_track_uri'].unique()
+        # unique_songs = df[['track_name', 'artist_name', 'artist_uri', 'track_uri', 'raw_track_uri', 'raw_artist_uri']]
+        # unique_songs = unique_songs['raw_track_uri'].unique()
+        #
+        # unique_artists = df[['artist_name', 'artist_uri', 'raw_artist_uri', 'track_name', 'track_uri', 'raw_track_uri']]
+        # unique_artists = unique_artists['raw_artist_uri'].unique()
 
-        unique_artists = df[['artist_name', 'artist_uri', 'raw_artist_uri', 'track_name', 'track_uri', 'raw_track_uri']]
-        unique_artists = unique_artists['raw_artist_uri'].unique()
+        unique_songs = df['raw_track_uri'].unique()
+        unique_artists = df['raw_artist_uri'].unique()
 
         sp = spotipy.Spotify(
             client_credentials_manager=SpotifyClientCredentials(client_id=Client_ID, client_secret=Client_Secret))
-        test_list = unique_songs[0:100]
-        len_song = len(unique_songs)
-        len_artists = len(unique_artists)
 
-        # There's definitely a better way to do this, but I'm too lazy to think about it.
-        splice_list = []
-        start = 0
-        stop = 100
-        while len_song > 0:
-            curr_splice = unique_songs[start:stop]
-            splice_list.append(curr_splice)
-            len_song = len_song - 100
-            start += 101
-            stop += 101
-            if start > len_song:
-                break
-            if stop > len_song:
-                stop = len_song
+        song_splices = list(generate_splices(unique_songs, 100))
+        song_splices_50 = list(generate_splices(unique_songs, 50))
+        artist_splices = list(generate_splices(unique_artists, 50))
 
-        # Awful part 2:
-        splice_artist_list = []
-        start_artist = 0
-        stop_artist = 50
-        while len_artists > 0:
-            curr_splice_artist = unique_artists[start_artist:stop_artist]
-            splice_artist_list.append(curr_splice_artist)
-            len_artists = len_artists - 50
-            start_artist += 51
-            stop_artist += 51
-            if start_artist > len_artists:
-                break
-            if stop_artist > len_artists:
-                stop_artist = len_artists
-
-        len_song = len(unique_songs)
-        track_feature_list = []
-        start = 0
-        stop = 50
-        while len_song > 0:
-            curr_splice_track = unique_songs[start:stop]
-            track_feature_list.append(curr_splice_track)
-            len_song = len_song - 50
-            start_artist += 51
-            stop_artist += 51
-            if start > len_song:
-                break
-            if stop > len_song:
-                stop = len_song
+        # Pulling song features, artist data, and audio features
+        print('Pulling Song Features')
+        track_features = pull_song_features(song_splices_50)
+        print('Pulling Artist Features')
+        artists = pull_artists(artist_splices)
+        print('Pulling Audio Features')
+        audio_features = pull_audio_features(song_splices)
 
         new_fname = fname.split('\\')
         new_fname = new_fname[1]
 
-        # test_splice = splice_list[0:2]
-        # test_artist_splice = splice_artist_list[0:2]
-        # test_track_splice = track_feature_list[0:2]
-        audio_features = pull_audio_features(splice_list=splice_list)
-        artists = pull_artists(splice_list=splice_artist_list)
-        track_features = pull_song_features(splice_list=track_feature_list)
+        # audio_features = pull_audio_features(splice_list=splice_list)
+        # artists = pull_artists(splice_list=splice_artist_list)
+        # track_features = pull_song_features(splice_list=track_feature_list)
 
         track_features = track_features.rename(columns={'name': 'track_name', 'popularity': 'track_popularity'})
 
         # Extract the followers value.
-        artists['followers'] = artists['followers'].apply(lambda x: x.get('total', 0) if isinstance(x, dict) else 0)
+        # artists['followers'] = artists['followers'].apply(lambda x: x.get('total', 0) if isinstance(x, dict) else 0)
 
         # Ensure the 'artists' and 'album' columns are in the correct format
         track_features['artists'] = track_features['artists'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
